4739fa172ab0d0d4756ed9743ba57d9b57f2e864
Michel Fernandes <michel.sousaf@gmail.com>
2018-06-18 20:53:36 -0300

    Endpoint - clientes-{id} disponivel

---
+++ src/main/java/com/michelfernandes/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.michelfernandes.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.michelfernandes.cursomc.domain.Cliente;
+import com.michelfernandes.cursomc.repositories.ClienteRepository;
+import com.michelfernandes.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id:" + id
+					+ ", Tipo:" + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
