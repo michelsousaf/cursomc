4c904731d3be40dc5207d30eac2845d634eac05f
Michel Fernandes <michel.sousaf@gmail.com>
2018-07-11 20:11:32 -0300

    Validacao sintatica com bean validation

--- src/main/java/com/michelfernandes/cursomc/services/CategoriaService.java
+++ src/main/java/com/michelfernandes/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.michelfernandes.cursomc.domain.Categoria;
+import com.michelfernandes.cursomc.dto.CategoriaDTO;
 import com.michelfernandes.cursomc.repositories.CategoriaRepository;
 import com.michelfernandes.cursomc.services.exceptions.DataIntegrityException;
 import com.michelfernandes.cursomc.services.exceptions.ObjectNotFoundException;
@@ -60,6 +61,10 @@ public class CategoriaService {
 	public  Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
