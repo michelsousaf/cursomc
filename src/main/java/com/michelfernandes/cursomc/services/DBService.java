1e99af93d2d5cc72241165f138093cf483ae9299
Michel Fernandes <michel.sousaf@gmail.com>
2018-07-29 10:40:58 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/michelfernandes/cursomc/services/DBService.java
+++ src/main/java/com/michelfernandes/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.michelfernandes.cursomc.domain.PagamentoComCartao;
 import com.michelfernandes.cursomc.domain.Pedido;
 import com.michelfernandes.cursomc.domain.Produto;
 import com.michelfernandes.cursomc.domain.enums.EstadoPagamento;
+import com.michelfernandes.cursomc.domain.enums.Perfil;
 import com.michelfernandes.cursomc.domain.enums.TipoCliente;
 import com.michelfernandes.cursomc.repositories.CategoriaRepository;
 import com.michelfernandes.cursomc.repositories.CidadeRepository;
@@ -162,18 +163,21 @@ public class DBService {
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "michel.sousaf@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "michel.sousa@live.com", "51149354020", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("27999323", "98888887"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
-
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-
+		Endereco e3 = new Endereco(null, "Avenida Matim afonso", "2018", null, "Centro", "1330787", cli2, c2);
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-
-		clienteRepository.save(Arrays.asList(cli1));
-
-		enderecoRepository.save(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
+		
+		clienteRepository.save(Arrays.asList(cli1,cli2));
+		enderecoRepository.save(Arrays.asList(e1, e2,e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
