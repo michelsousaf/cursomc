3fd17516b5a2259376c48a2df1bf62d8507f2af6
Michel Fernandes <michel.sousaf@gmail.com>
2018-08-04 11:05:57 -0300

    Esqueci a senha

--- src/main/java/com/michelfernandes/cursomc/services/EmailService.java
+++ src/main/java/com/michelfernandes/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.michelfernandes.cursomc.domain.Cliente;
 import com.michelfernandes.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -15,5 +16,7 @@ public interface EmailService {
 	void sendOderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void  sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
