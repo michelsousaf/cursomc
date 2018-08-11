b24d84f341369cc5d85f9dee20eda278a89f87b9
Michel Fernandes <michel.sousaf@gmail.com>
2018-08-11 13:13:30 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/michelfernandes/cursomc/services/ClienteService.java
+++ src/main/java/com/michelfernandes/cursomc/services/ClienteService.java
@@ -80,6 +80,20 @@ public class ClienteService {
 		enderecoRepository.save(obj.getEnderecos());
 		return obj;
 	}
+	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
 
 
 	public Cliente update(Cliente obj) {
